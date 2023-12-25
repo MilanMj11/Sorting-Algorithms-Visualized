8a875e733a677f803edc1faa296b473784260857
Mujdar Milan <mujdar.milan@gmail.com>
2023-12-25 21:22:02 +0200

    First Sorting done

--- Sorting Algorithms Visualized/main.cpp
+++ Sorting Algorithms Visualized/main.cpp
@@ -13,8 +13,19 @@ using namespace sf;
 int resolution_width = 1920;
 int resolution_height = 1080;
 
-const int N = 150;
+const int N = 200;
 vector<int> a(N);
+
+RectangleShape rectangle[N];
+
+float actual_display_width = resolution_width - 20;     /// 20 - represents the spaces to the edge of the screen
+float lines_and_spaces = actual_display_width / N;
+float line_width = 8.0 * (lines_and_spaces / 10.0);   /// 80% line
+float space_width = 2.0 * (lines_and_spaces / 10.0);  /// 20% space
+
+float actual_display_height = resolution_height - 20;
+float smallest_line = 5;
+float lines_height_diff = (actual_display_height - smallest_line) / (N - 1);
 
 void init_vector(vector<int>& a) {
     for (int i = 0; i < N; i++)
@@ -36,7 +47,47 @@ void print_vector(vector<int> &a) {
         cout << a[i] << ' ';
 }
 
+void Draw_BubbleSort(RenderWindow &window) {
+    
+    for (int i = 0; i < N-1; i++) {
+        for (int j = i+1; j < N; j++) {
+            if (a[i] > a[j]) {
+                swap(a[i], a[j]);
 
+                window.clear();
+                for (int t = 0; t < N; t++) {
+                    rectangle[t].setPosition(10 + a[t] * lines_and_spaces, resolution_height - 10);     /// Position on the X axis
+                    window.draw(rectangle[t]);
+                }
+                window.display();
+
+            }
+        }
+    }
+
+}
+
+bool CheckSorted(vector<int>& a) {
+    bool ok = true;
+    for (int i = 1; i < N; i++) {
+        if (a[i] <= a[i - 1]) {
+            ok = false;
+            break;
+        }
+    }
+    return ok;
+}
+
+void Draw_SortedAnimation(RenderWindow &window) {
+    //window.clear();
+    for (int i = 0; i < N; i++) {
+
+        rectangle[i].setFillColor(Color::Green);
+        window.draw(rectangle[i]);
+        window.display();
+    }
+
+}
 
 int main()
 {
@@ -46,17 +97,6 @@ int main()
 
 
     // ------------------------------------- CREATING LINES -----------------------------
-
-    RectangleShape rectangle[N];
-
-    float actual_display_width = resolution_width - 20;     /// 20 - represents the spaces to the edge of the screen
-    float lines_and_spaces = actual_display_width / N;
-    float line_width = 8.0 * ( lines_and_spaces / 10.0 );   /// 80% line
-    float space_width = 2.0 * ( lines_and_spaces / 10.0 );  /// 20% space
-
-    float actual_display_height = resolution_height - 20;
-    float smallest_line = 5;
-    float lines_height_diff = (actual_display_height - smallest_line) / (N - 1);
 
     for (int i = 0; i < N; i++) {
         rectangle[i].setSize(Vector2f(line_width,smallest_line + lines_height_diff * i)); /// dimensions of each line
@@ -80,6 +120,7 @@ int main()
     chrono::seconds duration(2);
     this_thread::sleep_for(duration);
 
+    bool sorted = false;
 
     while (window.isOpen()) {
         
@@ -91,12 +132,23 @@ int main()
 
         window.clear();
         
+        /*
         for (int i = 0; i < N; i++) {
             window.draw(rectangle[i]);
         }
+        */
+        if(CheckSorted(a) == false)
+            Draw_BubbleSort(window);
+        else {
+            if (sorted == false) {
+                Draw_SortedAnimation(window);
+                sorted = true;
+            }
+            for (int i = 0; i < N; i++) {
+                window.draw(rectangle[i]);
+            }
+        }
 
-
-        
         window.display();
 
     }

c9e0b221a764669ca14dc1979ff00c44ec1950df
Mujdar Milan <mujdar.milan@gmail.com>
2024-01-13 04:16:32 +0200

    quicksort done
    implementedd quicksort

--- Sorting Algorithms Visualized/headers/functions.h
+++ Sorting Algorithms Visualized/headers/functions.h
@@ -7,14 +7,21 @@ void print_vector(std::vector<int>& a);
 void reset_vector(std::vector<int>& a);
 bool CheckSorted(std::vector<int>& a);
 
+void destroy_references();
+
 void createLines();
 void createButtons();
 void createText();
 void Solve(int buttonNumber, sf::RenderWindow& window);
 void Draw_SortedAnimation(sf::RenderWindow& window);
+
 void Draw_BubbleSort(sf::RenderWindow& window);
 
 void Draw_MergeSort(sf::RenderWindow& window);
 void merge_sort(std::vector<int>& a, int st, int dr, sf::RenderWindow& window);
 void printVisualization(sf::RenderWindow& window);
 void concat(std::vector<int>& a, int st, int dr, sf::RenderWindow& window);
+
+int partitie(std::vector<int>& a, int low, int high, sf::RenderWindow& window);
+void quicksort(std::vector<int>& a, int low, int high, sf::RenderWindow& window);
+void Draw_QuickSort(sf::RenderWindow& window);
